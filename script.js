c956df66c08fa02efad77de72411f7225874b77f
manfry <48514287+manfry@users.noreply.github.com>
2026-09-09 17:46:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const CONFIG={
   // Formato: AAAA-MM-GGTHH:MM:SS (ora locale del dispositivo)
-  revealDate:"2026-09-09T18:00:00",
+  revealDate:"2026-09-09T20:15:00",
   // "male" oppure "female"
   gender:"female",
   texts:{
